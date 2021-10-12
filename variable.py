92f8a7a0e84c160a4072a00321153a20d7a7ba5e
Alphacodes <84385204+Alphacodes101@users.noreply.github.com>
2021-10-12 12:16:34 +0530

    var file

---
+++ variable.py
@@ -0,0 +1,17 @@
+x= 5 #int
+y="john"#string
+z=14.5
+x=str(3)
+print(x)
+print(y)
+print(z)
+#type cast
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#Type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
